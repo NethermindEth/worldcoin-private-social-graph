19eed0da85c36e460a455e02918419e3cc2a0ff1
MBelegris <mikeybel98@gmail.com>
2024-05-28 09:54:00 +0100

    fixed verify mt circuit tests

--- tests/integration.test.ts
+++ tests/integration.test.ts
@@ -2,31 +2,22 @@ import { Tree } from "../src/core/tree";
 import { verify_merkle_proof } from "../codegen";
 
 describe("Integration Tests", () => {
-    // TODO: fix `unwrap_throw` failed
     it("Should verify a merkle proof", async () => {
         const tree = new Tree()
         const idx = tree.addMember("101")
         const idx_proof = tree.generateMerkleProof(idx)
-        console.log(
-            "Proof: \n", 
-            "root:", idx_proof.root, "\n",
-            "leaf:", idx_proof.leaf, "\n",
-            "pathIndices:", idx_proof.pathIndices, "\n",
-            "pathIndices length:", idx_proof.pathIndices.length, "\n",
-            "siblings:", idx_proof.siblings, "\n",
-            "siblings length:", idx_proof.siblings.length, "\n",
-            "leafIndex:", idx_proof.leafIndex, "\n",
-        )
-        const leaf = idx_proof.leaf
-        const siblings = idx_proof.siblings
+
+        const leaf = idx_proof.leaf.toString()
+        const siblings = idx_proof.siblings.map(i => i.toString())
         const pathIndices = idx_proof.pathIndices.map(i => i.toString())
-        const root = idx_proof.root
+        const root = idx_proof.root.toString()
 
         const pass = await verify_merkle_proof(leaf, siblings, pathIndices, root)
 
         expect(pass).toBe(true)
     })
 
+    // TODO: verify pour circuit
     it("Should verify a pour transaction", () => {
 
     })
